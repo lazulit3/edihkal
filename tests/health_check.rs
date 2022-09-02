407275a4b6eee43e8d25a2befdab370d525acf34
lazulite <lazulite@riseup.net>
2022-09-01 21:15:55 -0700

    Simplify tests with axum-test-helper

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,24 +1,11 @@
-use axum::Server;
+use axum_test_helper::TestClient;
 use edihkal_server::router;
-use std::net::SocketAddr;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await;
-    let client = reqwest::Client::new();
+    let client = TestClient::new(router());
 
-    let response = client
-        .get("http://127.0.0.1:8080/health_check")
-        .send()
-        .await
-        .expect("Failed to request /health_check.");
+    let res = client.get("/health_check").send().await;
 
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
-async fn spawn_app() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    let server = Server::bind(&addr).serve(router().into_make_service());
-    let _ = tokio::spawn(server);
+    assert!(res.status().is_success());
 }
