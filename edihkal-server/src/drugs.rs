c5cc0182867503cb5ed81ccf717f0cb652b24aef
lazulite <lazulite@riseup.net>
2022-09-05 16:18:41 -0700

    Add beginnings of drugs route

--- edihkal-server/src/drugs.rs
+++ edihkal-server/src/drugs.rs
@@ -1,1 +1,15 @@
+use axum::{http::StatusCode, Json};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+pub struct Drug {
+    name: String,
+}
+
+pub async fn get_drugs() -> StatusCode {
+    StatusCode::OK
+}
+
+pub async fn define_drug(Json(payload): Json<Drug>) -> StatusCode {
+    StatusCode::OK
+}
--- edihkal-server/src/lib.rs
+++ edihkal-server/src/lib.rs
@@ -1,5 +1,15 @@
-use axum::{body::Body, http::StatusCode, routing::get, Router};
+pub mod drugs;
+use drugs::{define_drug, get_drugs};
+
+use axum::{
+    body::Body,
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
 
 pub fn router() -> Router<Body> {
-    Router::new().route("/health_check", get(|| async { StatusCode::OK }))
+    Router::new()
+        .route("/health_check", get(|| async { StatusCode::OK }))
+        .route("/drugs", get(get_drugs).post(define_drug))
 }
