4169404ecdf26127ed58ccff63ef6b08b78aa0fc
lazulite <lazulite@riseup.net>
2022-09-29 00:35:57 -0700

    refactor: Use State extractor instead of Extension

--- edihkal/src/drugs.rs
+++ edihkal/src/drugs.rs
@@ -1,6 +1,7 @@
-use std::sync::Arc;
-
-use axum::{http::StatusCode, Extension, Json};
+use axum::{
+    extract::{Json, State},
+    http::StatusCode,
+};
 use serde::Deserialize;
 use sqlx::PgPool;
 use uuid::Uuid;
@@ -14,10 +15,7 @@ pub async fn get_drugs() -> StatusCode {
     StatusCode::OK
 }
 
-pub async fn define_drug(
-    Extension(db_pool): Extension<Arc<PgPool>>,
-    Json(drug): Json<Drug>,
-) -> StatusCode {
+pub async fn define_drug(State(db_pool): State<PgPool>, Json(drug): Json<Drug>) -> StatusCode {
     match sqlx::query!(
         r#"
         INSERT INTO drugs (id, name)
@@ -26,7 +24,7 @@ pub async fn define_drug(
         Uuid::new_v4(),
         drug.name
     )
-    .execute(db_pool.as_ref())
+    .execute(&db_pool)
     .await
     {
         Ok(_) => StatusCode::OK,
--- edihkal/src/router.rs
+++ edihkal/src/router.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use axum::{body::Body, http::StatusCode, routing::get, Extension, Router};
+use axum::{http::StatusCode, routing::get, Router};
 use sqlx::PgPool;
 
 use crate::{
@@ -8,17 +8,16 @@ use crate::{
     drugs::{define_drug, get_drugs},
 };
 
-pub async fn app(configuration: &Settings) -> Router<Body> {
+pub async fn app(configuration: &Settings) -> Router<PgPool> {
     let db_pool = db_pool(&configuration.database).await;
     router(db_pool).await
 }
 
-pub async fn router(db_pool: PgPool) -> Router<Body> {
+pub async fn router(db_pool: PgPool) -> Router<PgPool> {
     let db_pool = Arc::new(db_pool);
-    Router::new()
+    Router::with_state_arc(db_pool)
         .route("/health_check", get(|| async { StatusCode::OK }))
         .route("/drugs", get(get_drugs).post(define_drug))
-        .layer(Extension(db_pool))
 }
 
 async fn db_pool(db_settings: &DatabaseSettings) -> PgPool {
