2e2ecdeaa19d9cd00966c9c0c5476b63f8105529
lazulite <lazulite@riseup.net>
2022-10-11 23:44:15 -0700

    feat(core): Drug::new for convenience w/ &str name

--- edihkal-core/src/drugs.rs
+++ edihkal-core/src/drugs.rs
@@ -4,3 +4,9 @@ use serde::{Deserialize, Serialize};
 pub struct Drug {
     pub name: String,
 }
+
+impl Drug {
+    pub fn new<S: Into<String>>(name: S) -> Drug {
+        Drug { name: name.into() }
+    }
+}
