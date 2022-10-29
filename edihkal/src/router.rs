53d374307bb90d262f6b8400a55c93894a47811d
lazulite <lazulite@riseup.net>
2022-10-28 19:35:23 -0700

    feat(srv): Add migrate() step to edihkal app start

--- edihkal/src/router.rs
+++ edihkal/src/router.rs
@@ -3,13 +3,17 @@ use std::sync::Arc;
 use axum::{body::Body, http::StatusCode, routing::get, Extension, Router};
 use sqlx::PgPool;
 
+use migration::{Migrator, MigratorTrait};
+
 use crate::{
     configuration::{DatabaseSettings, Settings},
     drugs::{define_drug, get_drugs},
 };
 
 pub async fn app(configuration: &Settings) -> Router<Body> {
-    let db_pool = db_pool(&configuration.database).await;
+    let db_settings = configuration.database;
+    let migrate(&db_settings);
+    let db_pool = db_pool(&db_settings).await;
     router(db_pool).await
 }
 
@@ -25,4 +29,12 @@ async fn db_pool(db_settings: &DatabaseSettings) -> PgPool {
     PgPool::connect(&db_settings.connection_string())
         .await
         .expect("Failed to connect to database")
+}
+
+/// Run database migrations
+async fn migrate(db_settings: &DatabaseSettings) {
+    let connection = sea_orm::Database::connect(&db_settings.connection_string())
+        .await
+        .unwrap();
+    Migrator::up(&connection, None).await.unwrap();
 }
