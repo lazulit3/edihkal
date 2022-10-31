5b26445bb551f73872c8a26821fd2741a5a5ad1a
lazulite <lazulite@riseup.net>
2022-10-30 19:23:05 -0700

    fix(srv): Make POST /drugs return created JSON

--- edihkal/src/drugs.rs
+++ edihkal/src/drugs.rs
@@ -11,28 +11,31 @@ pub async fn get_drugs() -> StatusCode {
 
 pub async fn define_drug(
     Extension(db_pool): Extension<Arc<PgPool>>,
-) -> StatusCode {
-    match sqlx::query!(
     Json(drug): Json<DrugInputs>,
+) -> Result<Json<Drug>, StatusCode> {
     // Construct Drug with random id from payload.
     let drug = Drug {
         id: Uuid::new_v4(),
         name: drug.name,
     };
+
+    match sqlx::query_as!(
+        Drug,
         r#"
         INSERT INTO drugs (id, name)
         VALUES ($1, $2)
+        RETURNING *
         "#,
         drug.id,
         drug.name
     )
-    .execute(db_pool.as_ref())
+    .fetch_one(db_pool.as_ref())
     .await
     {
-        Ok(_) => StatusCode::OK,
+        Ok(drug) => Ok(Json(drug)),
         Err(e) => {
             println!("Failed to execute query: {}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
         }
     }
 }
