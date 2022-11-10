b15afddec5e08e8501d63f383db99e394a1274cf
lazulite <lazulite@riseup.net>
2022-11-09 22:26:49 -0800

    chore: Use postgres for localdev via init_db.sh

--- edihkal/localdev/init_db.sh
+++ edihkal/localdev/init_db.sh
@@ -31,16 +31,16 @@ DB_NAME="${POSTGRES_DB:=edihkal}"
 DB_HOST="${POSTGRES_HOST:=127.0.0.1}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Set to skip container start if edihcal-timescaledb is already running
+# Set to skip container start if already running
 if [[ -z "${SKIP_STARTUP}" ]]
 then
-  "$CONTAINER_TOOL" run --name edihkal-timescaledb \
+  "$CONTAINER_TOOL" run --name edihkal-db \
              -p "127.0.0.1:${DB_PORT}:5432" \
              -e POSTGRES_DB=${DB_NAME} \
              -e POSTGRES_PASSWORD=${DB_PASSWORD} \
              -e POSTGRES_USER=${DB_USER} \
              -d \
-             timescale/timescaledb:latest-pg14
+             postgres:15-alpine
 fi
 
 # Wait until DB is ready
