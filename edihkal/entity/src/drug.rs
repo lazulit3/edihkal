5ee75f49b7fe0dd1d7fd20507c659fe6410fd044
lazulite <lazulite@riseup.net>
2022-11-12 15:17:50 -0800

    test: Add Drug unit tests from footgun thinking

--- edihkal/entity/src/drug.rs
+++ edihkal/entity/src/drug.rs
@@ -46,3 +46,94 @@ pub struct NewDrug {
     // id is ommitted for NewDrug requests
     pub name: String,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{ActiveModel, Model, NewDrug};
+    use sea_orm::{ActiveModelBehavior, ActiveModelTrait, IntoActiveModel, Set};
+
+    /// `NewDrug.into_active_model()` has expected values.
+    ///
+    /// This is the preferred approach to get an `ActiveModel` from a new drug request.
+    #[test]
+    fn new_drug_into_active_model_has_expected_values() {
+        let new_drug = NewDrug {
+            name: "Alcohol".to_owned(),
+        };
+        let active_model = new_drug.into_active_model();
+
+        assert!(active_model.id.is_set());
+        assert!(!active_model.id.as_ref().is_nil());
+        assert!(active_model.name.is_set());
+        assert_eq!(active_model.name.as_ref(), "Alcohol");
+    }
+
+    /// An `ActiveModel` constructed with `ActiveModel`'s `Default::default()` has expected values.
+    ///
+    /// Converting `NewDrug` may be preferred, but this is works.
+    #[test]
+    fn active_model_with_default_id_has_expected_values() {
+        let active_model = ActiveModel {
+            name: Set("Bufotenine".to_owned()),
+            ..Default::default()
+        };
+
+        assert!(!active_model.id.as_ref().is_nil());
+        assert!(active_model.id.is_set());
+        assert!(active_model.name.is_set());
+        assert_eq!(active_model.name.as_ref(), "Bufotenine");
+    }
+
+    /// An `ActiveModel` constructed with `ActiveModelBehavior::new()` has expected values.
+    ///
+    /// Converting `NewDrug` may be preferred, but this is works.
+    #[test]
+    fn active_model_new_has_expected_values() {
+        let mut active_model = ActiveModel::new();
+        active_model.name = Set("Cannabis".to_owned());
+
+        assert!(!active_model.id.as_ref().is_nil());
+        assert!(active_model.id.is_set());
+        assert!(active_model.name.is_set());
+        assert_eq!(active_model.name.as_ref(), "Cannabis");
+    }
+
+    // TODO: This is not a test.
+    /// A `drug::Model` with a `default()` `Uuid` converted into an `ActiveModel` has a nil `Uuid`.
+    ///
+    /// This is a footgun, but `ActiveModelBehavior::before_save()` can catch this.
+    #[test]
+    fn footgun_drug_model_with_default_id_into_active_model_has_nil_id() {
+        let drug_model = Model {
+            id: Default::default(),
+            name: "Dextrometorphan".to_owned(),
+        };
+        let active_model_via_into_trait = drug_model.into_active_model();
+
+        assert!(active_model_via_into_trait.id.as_ref().is_nil());
+
+        let drug_model = Model {
+            id: Default::default(),
+            name: "Dextrometorphan".to_owned(),
+        };
+        let active_model_via_from = ActiveModel::from(drug_model);
+
+        assert!(active_model_via_from.id.as_ref().is_nil());
+    }
+
+    // TODO: This is not a test.
+    /// Using `ActiveModelTrait::default()` for `id` results in a `NotSet` `id`.
+    ///
+    /// This is a footgun, but `ActiveModelBehavior::before_save()` can catch this.
+    #[test]
+    fn footgun_active_model_trait_default_for_id_is_not_set() {
+        let active_model = ActiveModel {
+            name: Set("Ephedrine".to_owned()),
+            ..ActiveModelTrait::default()
+        };
+
+        assert!(active_model.id.is_not_set());
+    }
+
+    // TODO: Test constraints around construction of drug::Model vs NewDrug vs ActiveModel
+}
