545c1beedffd8c92d8d08c07e4e20ea5ead038a4
lazulite <lazulite@riseup.net>
2022-11-20 23:49:03 -0800

    test(srv): Enable test tracing output via TEST_LOG

--- edihkal/tests/api/helpers/service.rs
+++ edihkal/tests/api/helpers/service.rs
@@ -1,11 +1,23 @@
 use std::net::TcpListener;
 
 use axum::Server;
-use edihkal::app::router;
+use once_cell::sync::Lazy;
 use sea_orm::DatabaseConnection;
 use tower::make::Shared;
 
+use edihkal::{app::router, tracing::configure_tracing};
+
 use super::unique_database;
+
+// Ensure that `tracing` is only initialized once.
+static TRACING: Lazy<()> = Lazy::new(|| {
+    // Use TEST_LOG to determine whether test logs should output.
+    if std::env::var("TEST_LOG").is_ok() {
+        configure_tracing("test", "info", std::io::stdout);
+    } else {
+        configure_tracing("test", "info", std::io::sink);
+    }
+});
 
 /// Contains details for connecting to an edihkal service and database used
 /// for integration testing.
@@ -21,6 +33,9 @@ impl TestService {
     /// The database connection points to a freshly created/migrated database with a unique name
     /// using the environment's configuration for connecting to a database.
     pub async fn new() -> TestService {
+        // Initialize `tracing` only if not already initialized.
+        Lazy::force(&TRACING);
+
         let listener = TcpListener::bind("127.0.0.1:0").expect("Could not bind ephemeral socket");
         let service_addr = listener.local_addr().unwrap();
         let service_url = format!("http://{}", service_addr);
