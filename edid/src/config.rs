78ea59f2421f4601706d6c33707a89f83a6ac80c
lazulite <lazulite@riseup.net>
2022-12-01 20:14:49 -0800

    fix!(cli): Fix edid config loading from file, envs
    BREAKING CHANGE: edid no longer provides --config for selecting the path
    of the configuration file to load; edid.toml (if present) and EDID_*
    environment variables are loaded with the latter taking precedence if
    settings are defined in both locations.
    
    If edihkal_url is not specified from either config provider, a default
    value is used: "http://localhost:8080"
    
    build(cli): Add figment for config from env & toml
    build(cli): Remove config dependency from edid

--- edid/src/config.rs
+++ edid/src/config.rs
@@ -1,53 +1,35 @@
 use anyhow::{Context, Result};
-use serde::Deserialize;
-use std::path::Path;
+use figment::{
+    providers::{Env, Format, Serialized, Toml},
+    Figment,
+};
+use serde::{Deserialize, Serialize};
 
-/// Configuration for edid loaded from `EDID_*` environment variables and an optional config file.
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Config {
     pub edihkal_url: String,
 }
 
-impl Config {
-    /// Returns `Config` loaded from a config file (if defined) and `EDID_*` environment
-    /// variables.
-    pub fn load(config_path: Option<&Path>) -> Result<Self> {
-        match config_path {
-            Some(config_path) => Config::load_from_config_file_and_envs(config_path),
-            None => Config::load_from_envs(),
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            edihkal_url: "http://localhost:8080".to_string(),
         }
-        .context("Failed to load configuration")
     }
+}
 
-    /// Returns `Config` loaded from `EDID_*` environment variables only.
-    fn load_from_envs() -> Result<Self> {
-        let config = config::Config::builder()
-            .add_source(
-                config::Environment::with_prefix("EDID")
-                    .prefix_separator("_")
-                    .separator("__"),
-            )
-            .build()?;
-        config
-            .try_deserialize::<Self>()
-            .context("Failed to deserialize configuration from environment variables")
-    }
-
-    /// Returns `Config` loaded from config file and `EDID_*` environment variables.
+impl Config {
+    /// Returns `Config` loaded from `edid.toml` and environment variables.
     ///
-    /// If a configuration is defined in both the config file and an environment variable,
-    /// the environment variable will take precedence.
-    fn load_from_config_file_and_envs(config_path: &Path) -> Result<Self> {
-        let config = config::Config::builder()
-            .add_source(config::File::from(config_path))
-            .add_source(
-                config::Environment::with_prefix("EDID")
-                    .prefix_separator("_")
-                    .separator("__"),
-            )
-            .build()?;
-        config.try_deserialize::<Self>().context(
-            "Failed to deserialize configuration from config file and environment variables",
-        )
+    /// If a setting is defined in both, environment variables take precedence.
+    ///
+    /// Environment variables are prefixed with `EDID_`; for example, to set the edihkal service URL,
+    /// the `EDID_EDIHKAL_URL` environment variable would be used.
+    pub fn load() -> Result<Self> {
+        Figment::from(Serialized::defaults(Config::default()))
+            .merge(Toml::file("edid.toml"))
+            .merge(Env::prefixed("EDID_"))
+            .extract()
+            .context("Failed to load configuration")
     }
 }
