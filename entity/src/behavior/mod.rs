908c8e571e889c23bd7cfa79e58b5fa1ecc69f14
lazulite <lazulite@riseup.net>
2023-04-07 22:01:19 -0700

    docs(entity): Add some docs to entity crate

--- entity/src/behavior/mod.rs
+++ entity/src/behavior/mod.rs
@@ -1,9 +1,14 @@
-//! [`behavior`] contains [`ActiveModelBehavior`][AMB] implementation for [Models][M].
+//! Contains [`ActiveModelBehavior`][AMB] and [`IntoCondition`][IC] implementations for `Model` types.
 //!
 //![`ActiveModelBehavior`][AMB] is implemented for each `Model` to ensure that an [`ActiveModel`s][AMT] are not inserted
-//! into the database with a [nil `Uuid`][uuid::Uuid::nil]
+//! into the database with a [nil `Uuid`][uuid::Uuid::nil].
 //!
-//! [AMT]: sea_orm::entity::ActiveModelTraot
+//! [`IntoCondition`][IC] is implemented for some `NewModel` types to create a query [`Condition`][C] for checking if an
+//! equivalent entity (matching all of `NewModel` fields) already exists in the database.
+//!
+//! [AMT]: sea_orm::entity::ActiveModelTrait
 //! [AMB]: sea_orm::entity::ActiveModelBehavior
+//! [C]: sea_orm::Condition
+//! [IC]: sea_orm::sea_query::IntoCondition
 pub mod drug;
 pub mod entry;
