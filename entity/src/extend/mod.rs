b832b645ad8c11195afdcfc0342384f2cd98ba8b
lazulite <lazulite@riseup.net>
2023-05-27 12:11:59 -0700

    feat(entity): Add `user` to `entity` crate

--- entity/src/extend/mod.rs
+++ entity/src/extend/mod.rs
@@ -1,9 +1,10 @@
-//! Contains [`ActiveModelBehavior`][AMB] implementations for `Model` types.
+//! Contains code extending the generated code for entities.
 //!
-//![`ActiveModelBehavior`][AMB] is implemented for each `Model` to ensure that [`ActiveModel`s][AMT] are not inserted
-//! into the database with a [nil `Uuid`][uuid::Uuid::nil].
+//! This includes [`ActiveModelBehavior`][AMB] implementations for each entity to ensure that
+//! [`ActiveModel`s][AMT] are not inserted into the database with a [nil `Uuid`][uuid::Uuid::nil].
 //!
 //! [AMT]: sea_orm::entity::ActiveModelTrait
 //! [AMB]: sea_orm::entity::ActiveModelBehavior
 pub mod drug;
 pub mod entry;
+pub mod user;
